aaef584e45b633a9045491cd6feda6688539fe0a
Rafael Gutiérrez <abadon.gutierrez@gmail.com>
2014-03-10 23:46:39 -0600

    A very simple excercise to iterate a collection and enable/disable a checkbox based on the value of one of the properties of the object in the collection

---
+++ ng-iterate/js/controllers.js
@@ -0,0 +1,12 @@
+var app = angular.module('app', []);
+ 
+app.controller('MainCtrl', function ($scope) {
+  $scope.things = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
